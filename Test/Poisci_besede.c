4463343a1f618cbb68ee2dad5821e40ecb93a01a
AndroAmater <molekula2@gmail.com>
2014-07-02 01:42:57 +0200

    Added Poisci_besede.c to main.c

--- Test/Poisci_besede.c
+++ Test/Poisci_besede.c
@@ -5,6 +5,8 @@
 //  Created by Andrej Fidel on 01/07/14.
 //  Copyright (c) 2014 Andrej Fidel. All rights reserved.
 //
+//http://www.putka.si/tasks/za_vajo/jajca1/izbor-besed
+//
 
 #include <stdio.h>
 #include <string.h>
@@ -13,9 +15,12 @@
 int poisciBes()
 {
     char a[2], b[256], *c;
+    printf("Vnesi črko in program ti bo izpisal vse besede v tvojem savku, ki se začnejo s to črko.\n");
     gets(a);
+    printf("Vnesi stavek.\n");
     gets(b);
     c=strtok(b, " ");
+    printf("\n\nIzpis besed:\n");
     while (c!=NULL) {
         if (c[0]==a[0]){
             printf("%s\n", c);
