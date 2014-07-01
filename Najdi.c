3558e642245ef436fa5b55bfd6b908643eb5364a
AndroAmater <molekula2@gmail.com>
2014-07-02 01:38:20 +0200

    Added najdi.c to main.c

--- Najdi.c
+++ Najdi.c
@@ -5,25 +5,30 @@
 //  Created by Andrej Fidel on 01/07/14.
 //  Copyright (c) 2014 Andrej Fidel. All rights reserved.
 //
+//http://www.putka.si/tasks/za_vajo/jajca1/najdi
+//
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 int najdi(){
-    char *a, b[128], c[64];
+    char *a, b[128], c[64], buf[128];
     int d=0, e=0, f=0;
+    printf("Vnesi iskano besedo oziroma del besede.\n");
     gets(c);
-    scanf("%d", &d);
+    printf("V koliko stavkih boš iskal?\n");
+    gets(buf);
+    sscanf(buf, "%d", &d);
     for (; e<=d; e++){
+        printf("Vnesi %d. stavek:\n", e+1);
         gets(b);
         a=strstr(b, c);
         while (a!=NULL){
             f++;
             a=strstr(a+1, c);
-                //printf("%d", f);
         }
     }
-    printf("%d\n", f);
+    printf("Najdenih je bilo %d besed \"%s\".\n", f, c);
     return 0;
 }
