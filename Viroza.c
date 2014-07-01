42d1a4a8943810e1b1f3478cf4d1ec51fa28070a
AndroAmater <molekula2@gmail.com>
2014-07-01 20:59:32 +0200

    Update
    Updated viroza.c for use with main.c

--- Viroza.c
+++ Viroza.c
@@ -8,10 +8,15 @@
 
 #include <stdio.h>
 
-int main(){
+int viroza(){
     int a=0, b=0, c=0, d=0, z=0, p=0;
-    scanf("%d", &b);
+    printf("Koliko kokoši je v kurniku?\n");
+    for (scanf("%d", &b); b%2!=0; scanf("%d", &b)) {
+        printf("Prosim unesi SODO ševilo!\n");
+    }
+    printf("Vpišite če je kokš zdrava(0) ali bolana(1) in program jih bo bo razporedil po kurnikih.\n");
     for (; a<b; a++) {
+        printf("%d. kokoš:  ", a+1);
         scanf("%d", &c);
         switch (c) {
             case 0:
@@ -20,18 +25,23 @@ int main(){
             case 1:
                 p++;
                 break;
+            default:
+                printf("Vnos nepravilen!\n");
+                a-=1;
+                break;
         }
     }
     d=b/2;
+    printf("\n");
     if (z>=d){
-        printf("%d 0 ", d);
+        printf("Zdrav kurnik: %d zdravih in 0 prehlajenih kokoši.\n", d);
         z-=d;
-        printf("%d %d\n", z, p);
+        printf("Prehlajen kurnik: %d zdravih in %d prehlajenih kokoši.\n", z, p);
     }
     else if (z<d){
-        printf("%d %d ", z, (d-z));
+        printf("Zdrav kurnik: %d zdravih in %d prehlajenih kokoši.\n", z, (d-z));
         p-=(d-z);
-        printf("0 %d\n", p);
+        printf("Prehlajen kurnik: 0 zdravih in %d prehlajenih kokoši.\n", p);
     }
     return 0;
 }
